aa2ea1955fb90d193ca656709fe99a5ebbb74315
Jeff <jeffrey.david.harris@gmail.com>
2015-02-20 09:06:25 -0500

    optimizing JS

--- quiz.js
+++ quiz.js
@@ -1,13 +1,20 @@
-
-
 $(function(){
     var jQuiz = {	
         init: function(){
 		quizScore = 0;
 		currentQues = 1;
-		quesNum = 1;
+		quesNum = 1;	
 		$whites = $('.whites');	
-		
+		$containerImg = $('.questionContainer img');
+		$next = $('.btnNext');
+		$scoreTwo = $('.scoreTwo');
+		$scoreText = $('.scoreTxt');
+		$quesLine = $('#quesLine');
+		$rightAns = $('.rightAnswer');
+		$wrongAns = $('.wrongAnswer');
+		$ansCont = $('.answerContainer');
+		$nextQues = $('.nxtQues');
+		$midAns = $('.midAnswer');
 		//Images	
 		theImages=['none',
 		'images/one.jpg',
@@ -25,51 +32,50 @@ $(function(){
 		greyImage = theGreyImages[quesNum];	
 		perfectScore = 'images/correct.jpg'
 		
-		$whites.fadeIn(1500);	
-		//Submit answer
-        $('.btnNext').click(function(){
-			curAnswer = $('input[type=radio]:checked:visible').attr('id');
-				if ($('input[type=radio]:checked:visible').length == 0) {        
-						return false;
-					}	
-				rad = $('input[type=radio]:checked:visible');	
-	            dAnswer = $('input[type=radio]:checked:visible').attr('class');
+//START QUIZ
+$whites.fadeIn(1500);	
 				
-              		
+//SUBMIT ANSWER FUNCTION
+        $next.click(function(){
+			$checked = $('input[type=radio]:checked:visible');
+			$curAnswer = $checked.attr('id');
+			//Check if radio button is checked
+			if ($checked.length == 0) {        
+					return false;
+			}		
+			//Fadeout white box and then show correct or incorrect
 				$.when($whites.fadeOut(500))
                         .done(function() {
-    							if (curAnswer == 'correct'){
-									$('.rightAnswer').show();
+    							if ($curAnswer == 'correct'){
+									$rightAns.show();
 									setTimeout(function(){runScore()},2000);
-									$('.nxtQues').show();
+									$nextQues.show();
 								}
-								if (curAnswer == 'wrong'){
-									$('.questionContainer img').attr('src',greyImage);
-									$('.wrongAnswer').show();
-									$('.nxtQues').show();
+								if ($curAnswer == 'wrong'){
+									$containerImg.attr('src',greyImage);
+									$wrongAns.show();
+									$nextQues.show();
 								}	
 								else {
 										if (quizScore == 100){
-											$('.scoreTxt').text('You got a perfect score!');
-											$('.questionContainer img').attr('src',perfectScore);	
-                                            $('.attribution').text('Peter Chadwick / WWF-Canon');											
+											$scoreText.text('You got a perfect score!');
+											$containerImg.attr('src',perfectScore);	 											
 										}
 										else{
-											$('.scoreTxt').text('You scored ' + quizScore + ' out of 100');	
+											$scoreText.text('You scored ' + quizScore + ' out of 100');	
 										}
-										var lastRad = $('input[type=radio]:checked:visible');
-											lastAnsText = lastRad.next('.ans').text(); 	
-										if (curAnswer == 'bonus'){
+										
+										if ($curAnswer == 'bonus'){
 										//Fire this for bonus question answer 1
-											$('.rightAnswer').show();	
+											$rightAns.show();	
 										}
-										if (curAnswer == 'noBonus'){
+										if ($curAnswer == 'noBonus'){
 										//Fire this for bonus question answer 1
-											$('.wrongAnswer').show();		
+											$wrongAns.show();		
 										}   
-										if (curAnswer == 'midBonus'){
+										if ($curAnswer == 'midBonus'){
 										//Fire this for bonus question answer 1
-											$('.midAnswer').show();	
+											$midAns.show();	
 										} 
 										if (quizScore == 60){
 											//Fire this if user scored 3/5
@@ -81,28 +87,28 @@ $(function(){
 											//Fire this if user scored 5/5
 										}	
 								}  
-    			$('.answerContainer').slideDown();  
+    			$ansCont.slideDown();  
 			});	
 		});		
 		
-		//Next Question
-			$('.nxtQues').click(function(){
+//NEXT QUESTION CLICK FUNCTION
+			$nextQues.click(function(){	
 				quesNum = quesNum + 1;
 				$('#quesTarget').text(quesNum);
 					$(this).parents('.questionContainer').fadeOut(500, function(){
-						$('.answerContainer').hide();
+						$ansCont.hide();
 						currentImage = theImages[quesNum];
 						greyImage = theGreyImages[quesNum];
 						$('.questionContainer img').attr('src',currentImage);
-						$('.rightAnswer').hide();
-						$('.wrongAnswer').hide();
+						$rightAns.hide();
+						$wrongAns.hide();
 						if(quesNum < 11){
 						$('#quesTarget').text(quesNum);
 						}
 						else{
-						$('#quesLine').text('Bonus Question');
+						$quesLine.text('Bonus Question');
 						}
-						$('.scoreTwo').css('color', '#ffffff');
+						$scoreTwo.css('color', '#ffffff');
 						
                     $(this).next('.questionContainer').fadeIn(500, function(){
 							$whites.fadeIn(1500);
@@ -111,7 +117,6 @@ $(function(){
 				});
 
 			$('.btnLst').click(function(){
-				$quesLine = $('#quesLine');
 					if (quizScore > 60){   
 						$quesLine.text('You\'re a genius!');											
 					}
@@ -119,40 +124,44 @@ $(function(){
 						$quesLine.text('Thanks for taking the quiz.');
 					}
 			});
-	
+			
+//RUN THE COUNTER WHEN USER SCORES POINTS FUNCTION			
+			function createCounter(elementId,start,end,totalTime,callback){
+				var jTarget=jQuery("#"+elementId);
+				var interval=totalTime/(end-start);
+				var intervalId;
+				var current=start;
+				var f=function(){
+					jTarget.text(current);
+					if(current==end){
+						clearInterval(intervalId);
+                    if(callback){
+                        callback();
+                    }
+				}
+				++current;
+				}
+				intervalId=setInterval(f,interval);
+				f();
+			}  
+
+//INCREASE SCORE WITH TALLY EFFECT FUNCTION
 			function runScore(){
-				$('.scoreTwo').css('color', '#6cc028');
+				$scoreTwo.css('color', '#6cc028');
 				createCounter("counterTarget",quizScore,quizScore + 10,500,function(){
 				quizScore = quizScore + 20;	
 			});
 			}
-          }
-    };
-    jQuiz.init();
+			
+//UNCHECK RADIO BUTTONS WHEN MULTIPLE ONES CHECKED FUNCTION
+			$radios =  $('.ans [type=radio]');
+			$radios.on('change',function(){
+				$radios.not(this).prop('checked',false);
+			});
+
+      } //END INIT
+    }; //CLOSE VAR
+	
+//FIRE QUIZ
+jQuiz.init();
 })
-
-//UNCHECK RADIO BUTTONS WHEN MULTIPLE ONES CLICKED
-$radios =  $('.ans [type=radio]');
-$radios.on('change',function(){
-		$radios.not(this).prop('checked',false);
-});
-
-//RUN THE COUNTER WHEN USER SCORES POINTS			
-function createCounter(elementId,start,end,totalTime,callback){
-      var jTarget=jQuery("#"+elementId);
-      var interval=totalTime/(end-start);
-      var intervalId;
-      var current=start;
-      var f=function(){
-           jTarget.text(current);
-           if(current==end){
-                clearInterval(intervalId);
-                    if(callback){
-                        callback();
-                         }
-            }
-            ++current;
-      }
-      intervalId=setInterval(f,interval);
-      f();
-}             
